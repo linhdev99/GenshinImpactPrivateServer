c027ce9397aa3df3c95488d9f1814a9f81932e45
DESKTOP-CKNLIRI\PeterLinh <linh.huynh.dev.99@gmail.com>
2022-05-25 18:45:39 +0700

    fix command languages

--- src/main/java/emu/grasscutter/BuildConfig.java
+++ src/main/java/emu/grasscutter/BuildConfig.java
@@ -2,6 +2,6 @@
         package emu.grasscutter;
         public class BuildConfig {
                 public static final String VERSION = "1.1.2-dev";
-                public static final String GIT_HASH = "00fd4d1";
+                public static final String GIT_HASH = "196377a";
         }
     
